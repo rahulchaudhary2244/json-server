002a68aa3319c782500c63a05edad452ea6400cc
Rahul Chaudhary <rahulchaudhary2244@gmail.com>
2025-04-21 11:31:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+server.use(router);
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
