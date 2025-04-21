438556d4d70608695fd30c4351120f7effad20ba
Rahul Chaudhary <rahulchaudhary2244@gmail.com>
2025-04-21 12:27:45 +0530

    commit

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('server.js hit');
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
